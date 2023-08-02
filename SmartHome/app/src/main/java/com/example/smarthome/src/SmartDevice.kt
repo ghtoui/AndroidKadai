d93cfcddd0abacbe4c3202dd2c5bdb38d8e12173
ghtoui <92294389+ghtoui@users.noreply.github.com>
2023-08-02 14:32:14 +0900

    SmartHomeセクション完成 #2 プロパティ委譲を少し理解できた。kotlinでは、setter, getterはその変数のprivateやpublicが着くから、自動生成のgetter, setterでは外部から値を取得できない 自動生成ではなくて、自分で決めたメソッドをpublicで作成してあげないと、外部から値を読み込めない

--- SmartHome/app/src/main/java/com/example/smarthome/src/SmartDevice.kt
+++ SmartHome/app/src/main/java/com/example/smarthome/src/SmartDevice.kt
@@ -1,4 +1,45 @@
 package com.example.smarthome.src
 
-class SmartDevice {
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
+
+open class SmartDevice (val name: String, val category: String) {
+    open protected val deviceType = ""
+    protected var deviceStatus = "off"
+
+    fun getDeviceStatus(): Boolean {
+        return when(deviceStatus) {
+            "on" -> true
+            else -> false
+        }
+    }
+    fun printDeviceInfo() {
+        println("Device name: $name, category: $category, type: $deviceType")
+    }
+
+    fun turnOn() {
+        deviceStatus = "on"
+        println("$name : online")
+    }
+    fun turnOff() {
+        deviceStatus = "off"
+        println("$name : offline")
+    }
+}
+class RangeRegulator(
+    initialValue: Int,
+    private val maxValue: Int,
+    private val minValue: Int
+) : ReadWriteProperty<Any?, Int> {
+    var fieldData = initialValue
+    override fun getValue(thisRef: Any?, property: KProperty<*>): Int {
+        return fieldData
+    }
+
+    override fun setValue(thisRef: Any?, property: KProperty<*>, value: Int) {
+        if (value in minValue..maxValue) {
+            println("$fieldData から$value へ変更しました")
+            fieldData = value
+        }
+    }
 }
